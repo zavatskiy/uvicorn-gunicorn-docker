000539107fa029d3373e40adba4b9844c92df877
zavatskiy <a.zavatskiy@gmail.com>
2023-05-11 17:32:42 +0300

    chore: improve gunicorn start script

--- docker-images/start.sh
+++ docker-images/start.sh
@@ -31,4 +31,4 @@ else
 fi
 
 # Start Gunicorn
-exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
+exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" --proxy-protocol --proxy-allow-from '*' --forwarded-allow-ips '*' "$APP_MODULE"
